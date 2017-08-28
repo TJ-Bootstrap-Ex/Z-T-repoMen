16c5a70054d152b0d9cf1dfae1783fc102cab476
zacharyramos <zachary.ramos9@gmail.com>
2017-08-28 13:24:13 -0500

    started loop for button display

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,25 @@
-// var btn = document.getElementsByClassName("btn");
-//
-// btn.addEventListener("click");
+var btn = document.getElementsByClassName("btn");
+
+btn.addEventListener("click");
 //
 //
 //
 // var something = document.querySelectorAll(".numb");
 // btn.addEventListener("onclick");
 
-var screenLeft = document.querySelector(".left-operand");
-var screenMid = document.querySelector(".operator");
-var screenRight = document.querySelector(".result");
+var inputLeft = document.querySelector(".left-operand");
+var inputMid = document.querySelector(".operator");
+var inputRight = document.querySelector(".result");
+var numbers = document.querySelectorAll(".numb");
 
 
+for  (var i = 0; i < numbers.length; i++) {
+    numbers[i].onClick = function (){
+
+    }
+}
+
+// loop buttons
+// add an event listener to each one
+
+//var onClick = document.
