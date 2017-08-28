5e34b1f86ca19e3fc1f074ee41b5fa35cf852cb1
zacharyramos <zachary.ramos9@gmail.com>
2017-08-28 11:33:23 -0500

    still working on functionality

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,14 @@
-var btn = document.getElementsByClassName("btn");
-
-btn.addEventListener("click");
-
-// var x = document.querySelectorAll("btn");
+// var btn = document.getElementsByClassName("btn");
 //
-// x
+// btn.addEventListener("click");
+//
+//
+//
+// var something = document.querySelectorAll(".numb");
+// btn.addEventListener("onclick");
+
+var screenLeft = document.querySelector(".left-operand");
+var screenMid = document.querySelector(".operator");
+var screenRight = document.querySelector(".result");
+
+
