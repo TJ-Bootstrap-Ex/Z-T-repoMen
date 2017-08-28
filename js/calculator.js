3fe56f9684ab373fbb5b0c628df5b4ffa7eb092a
Elvin <ttop34@icloud.com>
2017-08-28 14:38:12 -0500

    created divs to display buttons and operators

--- js/calculator.js
+++ js/calculator.js
@@ -1,25 +1,209 @@
-var btn = document.getElementsByClassName("btn");
-
-btn.addEventListener("click");
+// window.onload (){
 //
 //
+//     var btn = document.getElementsByClassName("btn");
 //
-// var something = document.querySelectorAll(".numb");
-// btn.addEventListener("onclick");
+// btn.addEventListener("click");
+// //
+// //
+// //
+// // var something = document.querySelectorAll(".numb");
+// // btn.addEventListener("onclick");
+//
+// var inputLeft = document.querySelector(".left-operand");
+// var inputMid = document.querySelector(".operator");
+// var inputRight = document.querySelector(".result");
+// var numbers = document.querySelectorAll(".numb");
+//
+//
+// for  (var i = 0; i < numbers.length; i++) {
+//     numbers[i].onClick = function (){
+//         var input = document.
+//     }
+// }
+//
+// // loop buttons
+// // add an event listener to each one
+//
+// //var onClick = document.
+// }
+window.onload = function () {
 
-var inputLeft = document.querySelector(".left-operand");
-var inputMid = document.querySelector(".operator");
-var inputRight = document.querySelector(".result");
-var numbers = document.querySelectorAll(".numb");
+
+    var current,
+        screen,
+        output,
+        limit,
+        zero,
+        period,
+        operator,
+        midScreen,
+        lastScreen;
 
 
-for  (var i = 0; i < numbers.length; i++) {
-    numbers[i].onClick = function (){
+    screen = document.getElementById("left-operand");
+    midScreen = document.getElementById("operator");
+    lastScreen = document.getElementById("result");
+
+
+    var elem = document.querySelectorAll(".numb");
+    var op = document.querySelectorAll(".oper");
+    var elemTwo = document.querySelectorAll(".num");
+
+    var oplen = op.length;
+    var len = elem.length;
+
+
+
+
+    for(var i = 0; i < len; i++ ) {
+
+        elem[i].addEventListener("click",function() {
+
+            num = this.value;
+
+            output = screen.innerHTML += num;
+
+            // limit = output.length;
+
+            if(limit > 16 ) {
+
+                alert("Sorry no more input is allowed");
+
+            }
+
+        },false);
+
+    }
+
+var lastField = function () {for(var i = 0; i < len; i++ ) {
+
+    elemtwo[i].addEventListener("click",function() {
+
+        num = this.value;
+
+        output = lastScreen.innerHTML += num;
+
+        // limit = output.length;
+
+        if(limit > 16 ) {
+
+            alert("Sorry no more input is allowed");
+
+        }
+
+    },false);
+
+}};
+
+
+
+
+
+
+    for(var i = 0; i < len; i++ ) {
+
+        op[i].addEventListener("click",function() {
+
+            num = this.value;
+
+            output = midScreen.innerHTML += num;
+
+            // limit = output.length;
+
+            if(limit > 16 ) {
+
+                alert("Sorry no more input is allowed");
+
+            }
+
+        },false);
+
+    }
+
+    if (numbers &&){
+
+    }
+
+    document.querySelector(".zero").addEventListener("click",function() {
+
+        zero = this.value;
+
+        if(screen.innerHTML === "") {
+
+            output = screen.innerHTML = zero;
+        }
+
+        else if(screen.innerHTML === output) {
+
+            output = screen.innerHTML +=zero;
+
+        }
+
+    },false);
+
+    document.querySelector(".period").addEventListener("click",function() {
+
+        period = this.value;
+
+        if(screen.innerHTML === "") {
+
+            output = screen.innerHTML = screen.innerHTML.concat("0.");
+
+        }
+
+        else if(screen.innerHTML === output) {
+
+            screen.innerHTML = screen.innerHTML.concat(".");
+
+        }
+
+    },false);
+
+
+    document.querySelector("#eqn-bg").addEventListener("click",function() {
+
+        if(screen.innerHTML === output) {
+
+            screen.innerHTML = eval(output);
+        }
+
+        else {
+            screen.innerHTML = "";
+        }
+
+    },false);
+
+    document.querySelector("#clear").addEventListener("click",function() {
+
+        screen.innerHTML = "";
+
+    },false);
+
+
+    var elem1 = document.querySelectorAll(".operator");
+
+    var len1 = elem1.length;
+
+    for(var i = 0; i < len1; i++ ) {
+
+        elem1[i].addEventListener("click",function() {
+
+            operator = this.value;
+
+            if(screen.innerHTML === "") {
+
+                screen.innerHTML = screen.innerHTML.concat("");
+
+            }
+
+            else if(output) {
+
+                screen.innerHTML = output.concat(operator);
+
+            }
+
+        },false);
 
     }
 }
-
-// loop buttons
-// add an event listener to each one
-
-//var onClick = document.
